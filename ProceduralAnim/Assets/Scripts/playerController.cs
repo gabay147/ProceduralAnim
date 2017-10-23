8b7a9bcf052382ce59fb424757aa5804319c92d5
LibSTC163User <libstc163user@labs.salina.k-state.edu>
2017-10-23 11:56:41 -0500

    added Unity project file, imported select standard assets and free 'grids and nets' materials package from the asset store, added player gameObject, and started playerController script.

---
+++ ProceduralAnim/Assets/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Collider collider;
+	public float speed;
+	public float curVelocity;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		collider = GetComponent<Collider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputZ = Input.GetAxisRaw ("Vertical");
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		Vector3 deltaForce = new Vector3 (inputX, 0.0f, inputZ);
+		rb.AddForce (deltaForce * speed, ForceMode.Impulse);
+		curVelocity = rb.velocity.magnitude;
+	}
+}
